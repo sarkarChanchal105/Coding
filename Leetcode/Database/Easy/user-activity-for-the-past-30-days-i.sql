cd818cf611672f47cdbc9b910dc83ee0d268d109
Chanchal Sarkar <csarkar@Chanchals-MacBook-Pro-2.local>
2020-03-02 06:30:37 -0600

    Added the SQL file

---
+++ Leetcode/Database/Easy/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,69 @@
+/*
+https://leetcode.com/problems/user-activity-for-the-past-30-days-i/
+
+Table: Activity
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| session_id    | int     |
+| activity_date | date    |
+| activity_type | enum    |
++---------------+---------+
+There is no primary key for this table, it may have duplicate rows.
+The activity_type column is an ENUM of type ('open_session', 'end_session', 'scroll_down', 'send_message').
+The table shows the user activities for a social media website.
+Note that each session belongs to exactly one user.
+
+
+Write an SQL query to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on some day if he/she made at least one activity on that day.
+
+The query result format is in the following example:
+
+Activity table:
++---------+------------+---------------+---------------+
+| user_id | session_id | activity_date | activity_type |
++---------+------------+---------------+---------------+
+| 1       | 1          | 2019-07-20    | open_session  |
+| 1       | 1          | 2019-07-20    | scroll_down   |
+| 1       | 1          | 2019-07-20    | end_session   |
+| 2       | 4          | 2019-07-20    | open_session  |
+| 2       | 4          | 2019-07-21    | send_message  |
+| 2       | 4          | 2019-07-21    | end_session   |
+| 3       | 2          | 2019-07-21    | open_session  |
+| 3       | 2          | 2019-07-21    | send_message  |
+| 3       | 2          | 2019-07-21    | end_session   |
+| 4       | 3          | 2019-06-25    | open_session  |
+| 4       | 3          | 2019-06-25    | end_session   |
++---------+------------+---------------+---------------+
+
+Result table:
++------------+--------------+
+| day        | active_users |
++------------+--------------+
+| 2019-07-20 | 2            |
+| 2019-07-21 | 2            |
++------------+--------------+
+Note that we do not care about days with zero active users.
+
+*/
+
+/* Write your PL/SQL query statement below */
+
+select * from
+(
+select to_char(activity_date,'YYYY-MM-DD') day, count(distinct user_id) active_users
+from Activity
+WHERE TO_DATE('2019-07-27','yyyy-mm-dd')-activity_date<30
+group by activity_date;
+) where active_users>0
+
+order by 1
+
+select distinct viewer_id as "id"
+
+from views
+group by view_date, viewer_id
+having count(distinct article_id)>1
+order by 1
---
+++ Leetcode/python/Easy/how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,78 @@
+"""
+
+https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+
+
+
+Example 1:
+
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation:
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1).
+For nums[3]=2 there exist one smaller number than it (1).
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+Example 2:
+
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+Example 3:
+
+Input: nums = [7,7,7,7]
+Output: [0,0,0,0]
+
+
+Constraints:
+
+2 <= nums.length <= 500
+0 <= nums[i] <= 100
+"""
+
+
+class Solution:
+    def smallerNumbersThanCurrent(self, nums):
+        nums1=sorted(nums,reverse=True)
+        out=[]
+        dict={}
+        n=len(nums1)
+        for i in range(n):
+            dict[nums1[i]]=n-(i+1)
+
+        #print (dict)
+
+        for a in nums:
+            out.append(dict[a])
+
+        return out
+
+
+# class Solution:
+#     def smallerNumbersThanCurrent(self, nums):
+#         counts = [0] * 101
+#         print(counts)
+#         for a in nums:
+#             counts[a] += 1
+#         print("\n")
+#         print(counts)
+#         for i in range(1, 101):
+#             counts[i] += counts[i - 1]
+#             res = [0] * len(nums)
+#         print("\n")
+#         print(res)
+#         for i, a in enumerate(nums):
+#             if a > 0:
+#                 res[i] = counts[a - 1]
+#         return res
+
+
+
+nums=[8,1,2,2,3]
+object=Solution()
+
+print(object.smallerNumbersThanCurrent(nums)
+      )
